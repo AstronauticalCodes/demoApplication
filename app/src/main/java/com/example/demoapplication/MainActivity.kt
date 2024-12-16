e97672e25ded368c2985337887178308876e7dfe
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-16 17:50:49 +0530

    Image composable function.

--- app/src/main/java/com/example/demoapplication/MainActivity.kt
+++ app/src/main/java/com/example/demoapplication/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.demoapplication.ui.theme.DemoApplicationTheme
 import androidx.compose.ui.unit.sp
@@ -43,32 +46,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
+//    Box(
+////            .fillMaxSize()
+////            .fillMaxWidth()
+//    ) {
+//        Text(
+//            text = "Hello $name!",
+//            fontSize = 40.sp,
+//            modifier = Modifier
+//                .background(Color.Red)
+//                .padding(16.dp)
+//                .background(Color.Gray)
+//                .align(Alignment.Center)
+////                .fillMaxSize()
+//        )
+//        Text(
+//            text = "asdf",
+//            fontSize = 40.sp,
+//            modifier = Modifier
+//                .background(Color.Red)
+//                .padding(16.dp)
+//                .background(Color.Gray)
+//                .align(Alignment.BottomCenter)
+//        )
+//    }
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = null,
         modifier = Modifier
-            .size(600.dp)
-//            .fillMaxSize()
-//            .fillMaxWidth()
-    ) {
-        Text(
-            text = "Hello $name!",
-            fontSize = 40.sp,
-            modifier = Modifier
-                .background(Color.Red)
-                .padding(16.dp)
-                .background(Color.Gray)
-//                .fillMaxSize()
-        )
-        Text(
-            text = "asdf",
-            fontSize = 40.sp,
-            modifier = Modifier
-                .background(Color.Red)
-                .padding(16.dp)
-                .background(Color.Gray)
-        )
-    }
+            .background(Color.Green)
+    )
 }
 
 @Preview(showBackground = true)
