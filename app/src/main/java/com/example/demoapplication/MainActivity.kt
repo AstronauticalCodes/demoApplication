2a90aa332d4216517388449b525f8c1b4fc43d2b
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-16 17:38:54 +0530

    Basic Column composable function.

--- app/src/main/java/com/example/demoapplication/MainActivity.kt
+++ app/src/main/java/com/example/demoapplication/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,14 +43,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        fontSize = 40.sp,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            .background(Color.Red)
-            .padding(16.dp)
-            .background(Color.Gray)
-    )
+            .size(400.dp)
+//            .fillMaxSize()
+//            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 40.sp,
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(16.dp)
+                .background(Color.Gray)
+//                .fillMaxSize()
+        )
+        Text(
+            text = "asdf",
+            fontSize = 40.sp,
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(16.dp)
+                .background(Color.Gray)
+        )
+    }
 }
 
 @Preview(showBackground = true)
