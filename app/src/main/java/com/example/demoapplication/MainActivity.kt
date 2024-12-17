c0ab1fd5242bb32f25b941a8c0fd1cb28ac065cf
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-17 20:07:25 +0530

    Cleaned the code (Removed warnings)

--- app/src/main/java/com/example/demoapplication/MainActivity.kt
+++ app/src/main/java/com/example/demoapplication/MainActivity.kt
@@ -1,43 +1,26 @@
 package com.example.demoapplication
 
-import android.graphics.fonts.SystemFonts
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.demoapplication.ui.theme.DemoApplicationTheme
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 
 
@@ -47,10 +30,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DemoApplicationTheme {
-//                SystemFonts.getAvailableFonts().forEach {
-//                    Log.d("jeloo", "onCreate: ${it.}")
-//                }
-//                Log.d("jeloo", "onCreate: ${SystemFonts.getAvailableFonts()}")
                 var name = remember {
                     mutableStateOf("")
                 }
@@ -60,7 +39,6 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color.Gray)
                         .padding(16.dp)
                         .padding(top = 60.dp)
                 ) {
@@ -97,7 +75,9 @@ class MainActivity : ComponentActivity() {
                                     .fillMaxWidth()
                                     .padding(16.dp)
                             )
-                            Divider()
+                            Divider(
+                                Modifier, 1.dp, androidx.compose.ui.graphics.Color.Red
+                            )
                         }
                     }
                 }
